a3d243ab2da98bad0e4482d6028d5b0127b025a9
stiven <stivenaig@gmail.com>
2021-03-22 20:31:36 -0500

    commit primera version

--- presentacion/curso/modalCurso.php
+++ presentacion/curso/modalCurso.php
@@ -10,7 +10,7 @@ $curso -> consultar();
 		<tbody>
 			<tr><th width="20%">Nombre</th><td><?php echo $curso -> getNombre(); ?></td></tr>	
 			<tr><th width="20%">Descripcion</th><td><?php echo $curso -> getDescripcion(); ?></td></tr>
-			<tr><th width="20%">Autor</th><td><a><?php echo $curso -> getAutor(); ?></a></td></tr>
+			<tr><th width="20%">Autor</th><td><?php echo $curso -> getAutor(); ?></td></tr>
 			<tr><th width="20%">Link</th><td><a><?php echo $curso -> getLink(); ?></a></td></tr>
 		<?php if($curso -> getFechaApertura() !="0000-00-00"){ ?>	<tr><th width="20%">Fecha Apertura</th><td><?php echo $curso -> getFechaApertura(); ?></td></tr><?php } ?>
 		<?php if($curso -> getFechaCierre() !="0000-00-00"){ ?>	<tr><th width="20%">Fecha Cierre</th><td><?php echo $curso -> getFechaCierre(); ?></td></tr><?php } ?>
